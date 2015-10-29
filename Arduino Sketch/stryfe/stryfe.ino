e7553c17ed195d941ae5e2c6028475f37be6258d
Micha Krause <micha@krausam.de>
2015-10-29 23:03:59 +0100

    x

--- Arduino Sketch/stryfe/stryfe.ino
+++ Arduino Sketch/stryfe/stryfe.ino
@@ -1,54 +1,103 @@
-const int motor_min = 15; // Lowest Motor Speed possible
-const int read_write_factor = 1020 / (255 - motor_min);
-
 const int read_pin    = 3;
 const int write_pin   = 1;
 const int trigger_pin = 0;
+const int motor_normal_speed = 100;
 
 boolean was_low = false;
-int     poti    = 0;
-int     motor   = 255;
+unsigned long last_trigger = 0;
+int     last_trigger_time;
 
-void beep() {
-    analogWrite(write_pin, 10);
-    delay(100);
+
+void beep(int number) {
+  for (int x=0;x<number;x++) {
     analogWrite(write_pin, 0);
     delay(100);
-}  
+    analogWrite(write_pin, 10);
+    delay(100);
+  }
+  analogWrite(write_pin, 0);
+}
+
+int get_motor(int poti) {
+  if      (poti < 3) return 16;
+  else if (poti == 3) return 0;
+  else if (poti < 33) return 19;
+  else if (poti == 33) return 0;
+  else if (poti < 105) return 22;
+  else if (poti == 105) return 0;
+  else if (poti < 190) return 25;
+  else if (poti == 190) return 0;
+  else if (poti < 280) return 31;
+  else if (poti == 280) return 0;
+  else if (poti < 370) return 39;
+  else if (poti == 370) return 0;
+  else if (poti < 450) return 48;
+  else if (poti == 450) return 0;
+  else if (poti < 615) return 61;
+  else if (poti == 615) return 0;
+  else if (poti < 680) return 76;
+  else if (poti == 680) return 0;
+  else if (poti < 780) return 95;
+  else if (poti == 780) return 0;
+  else if (poti < 880) return 119;
+  else if (poti == 880) return 0;
+  else if (poti < 915) return 149;
+  else if (poti == 915) return 0;
+  else if (poti < 950) return 186;
+  else if (poti == 950) return 0;
+} 
 
 void setup()
 {
+  int     poti    = 0;
   pinMode(write_pin,   OUTPUT);   // sets the pin as output
   pinMode(trigger_pin, INPUT_PULLUP);
 
   poti  = analogRead(read_pin);
-  if (poti > 1020) {
-    beep();
-    beep();
+
+  if (poti <= 950) {
+    analogWrite(write_pin,255);
+    delay(get_motor(poti)*6);
   }
   else {
-    analogWrite(write_pin,255);
-    motor = motor_min + (poti / read_write_factor);
-    delay(motor*4);
+    beep(2);
   }
 }
 
-void loop()
-{
+void loop() {
+  int     poti    = 0;
+  int motor = 0;
+
   poti = analogRead(read_pin);
-  if (poti < 1020) {
+
+  if (poti <= 950) {
     if (digitalRead(trigger_pin) == false) was_low = true;
     if (digitalRead(trigger_pin) && was_low) {
       analogWrite(write_pin,255);
-      delay(50);
+      delay(45);
       was_low = false;
-    } 
-    motor = motor_min + (poti / read_write_factor);
+    }
+    motor=get_motor(poti);
+    analogWrite(write_pin, motor);
+    if (!motor) delay(200);
   }
   else {
-    if (digitalRead(trigger_pin)) motor = 0;
-    else motor = 255;
+    if (digitalRead(trigger_pin) == false && was_low == false) {
+      analogWrite(write_pin,255);
+      last_trigger_time = millis() - last_trigger;            // calculate milliseconds betwen this trigger pull and the last one
+      if (last_trigger_time > 5000) {
+        delay(200);
+      }
+      else {
+        delay((last_trigger_time / 50));                        // Run motor dependend on guessed remaining flywheel speed, and mode
+      }
+      analogWrite(write_pin,motor_normal_speed);
+      was_low = true;
+    }
+    else if (digitalRead(trigger_pin) && was_low) {
+      last_trigger = millis();
+      was_low = false;
+      analogWrite(write_pin,0);
+    }
   }
-  if (motor > 255) motor = 255;
-  analogWrite(write_pin, motor);  // analogRead values go from 0 to 1023, analogWrite values from 0 to 255
 }
